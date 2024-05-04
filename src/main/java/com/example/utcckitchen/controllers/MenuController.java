629036f9e7ce4ed19f1228f0a8e042bcdef78b82
pichetphong <pichetphong@gmail.com>
2024-05-04 19:02:52 +0700

    Create menu function

--- src/main/java/com/example/utcckitchen/controllers/MenuController.java
+++ src/main/java/com/example/utcckitchen/controllers/MenuController.java
@@ -3,12 +3,24 @@ package com.example.utcckitchen.controllers;
 import com.example.utcckitchen.models.Menu;
 import com.example.utcckitchen.models.MenuDto;
 import com.example.utcckitchen.services.MenuRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -30,5 +42,51 @@ public class MenuController {
         MenuDto menuDto = new MenuDto();
         model.addAttribute("menuDto", menuDto);
         return "menus/CreateMenu";
+    }
+
+    @PostMapping("/create")
+    public String createMenu(
+            @Valid @ModelAttribute MenuDto menuDto,
+            BindingResult result
+    ){
+        if (menuDto.getImageFile().isEmpty()){
+            result.addError(new FieldError("menuDto", "imageFile", "The image file is required"));
+        }
+
+        if (result.hasErrors()){
+            return "menus/CreateMenu";
+        }
+
+        // save image
+        MultipartFile image = menuDto.getImageFile();
+        Date createdAt = new Date();
+        String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
+
+        try {
+            String uploadDir = "public/images/";
+            Path uploadPath = Paths.get(uploadDir);
+
+            if (!Files.exists(uploadPath)){
+                Files.createDirectories(uploadPath);
+            }
+
+            try (InputStream inputStream = image.getInputStream()){
+                Files.copy(inputStream, Paths.get(uploadDir + storageFileName),
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (Exception ex) {
+            System.out.println("Exception: "+ ex.getMessage());
+        }
+
+        Menu menu = new Menu();
+        menu.setName(menuDto.getName());
+        menu.setPrice(menuDto.getPrice());
+        menu.setDescription(menuDto.getDescription());
+        menu.setCreatedAt(createdAt);
+        menu.setImageFileName(storageFileName);
+
+        repo.save(menu);
+
+        return "redirect:/menus";
     }
 }
--- src/main/java/com/example/utcckitchen/models/Customer.java
+++ src/main/java/com/example/utcckitchen/models/Customer.java
@@ -10,8 +10,9 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
     private String name;
+    private String student_id;
+    private String phone_number;
 
     public int getId() {
         return id;
@@ -27,5 +28,21 @@ public class Customer {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(String student_id) {
+        this.student_id = student_id;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
     }
 }
